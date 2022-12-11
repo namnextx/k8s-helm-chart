a95f4e9516557bb439a788fc5084f0f5db7f4ae1
Nam Phuong <phuongnam.nextx@gmail.com>
2022-12-11 17:54:17 +0700

    fix yaml file

--- push_chart.sh
+++ push_chart.sh
@@ -3,7 +3,7 @@ echo "Begin"
 FILES="$1/*.tgz"
 export isChartChange=0
 
-echo "GITHUG_TOKEN" | helm registry login -u ${GITHUG_TOKEN} --password-stdin ${CHART_REGISTRY}
+echo "GITHUG_TOKEN" | helm registry login -u ${namnextx} --password-stdin ${CHART_REGISTRY}
 for f in $FILES
 do
     echo "$f Processing"
