7b8b876d80f6e1502a8bdc5ca29cf2d8adcf0f5b
Nam Phuong <phuongnam.nextx@gmail.com>
2022-12-15 12:12:51 +0700

    update readme file

--- push_chart.sh
+++ push_chart.sh
@@ -8,7 +8,7 @@ echo "$GITHUB_TOKEN" | helm registry login -u namnextx --password-stdin ${CHART_
 for f in $FILES
 do
     echo "$f Processing"
-    echo helm push $f oci://${CHART_REGISTRY}/${OWNER_REPO}
+    echo "helm push" | helm push $f oci://${CHART_REGISTRY}/${OWNER_REPO}
     export isChartChange=1
 done
 
