f2b757df186eec8bb178d4d4372ba0bc0ff73bca
Nam Phuong <phuongnam.nextx@gmail.com>
2022-12-19 22:55:52 +0700

    update readme file

--- push_chart.sh
+++ push_chart.sh
@@ -16,21 +16,21 @@ do
     export isChartChange=1
 done
 
-echo $isChartChange
-
-if [ $isChartChange = 1 ];
-then
-    echo "Updating the index.yaml..."
-    git remote set-url origin "https://${GITHUB_TOKEN}:${GITHUB_TOKEN}@github.com/${OWNER_REPO}/${REPO_NAME}"
-    git reset --hard
-    git checkout chart-releases
-    git pull
-    helm repo index ${CHART_PACKAGE_PATH} --merge index.yaml --url oci://${CHART_REGISTRY}/${OWNER_REPO}
-    mv -f "${CHART_PACKAGE_PATH}/index.yaml" "index.yaml"
-    git add index.yaml
-    git commit -m "Update index..."
-    git push
-else
-    echo "There is no chart to be updated."
-fi
+#echo $isChartChange
+#
+#if [ $isChartChange = 1 ];
+#then
+#    echo "Updating the index.yaml..."
+#    git remote set-url origin "https://${GITHUB_TOKEN}:${GITHUB_TOKEN}@github.com/${OWNER_REPO}/${REPO_NAME}"
+#    git reset --hard
+#    git checkout chart-releases
+#    git pull
+#    helm repo index ${CHART_PACKAGE_PATH} --merge index.yaml --url oci://${CHART_REGISTRY}/${OWNER_REPO}
+#    mv -f "${CHART_PACKAGE_PATH}/index.yaml" "index.yaml"
+#    git add index.yaml
+#    git commit -m "Update index..."
+#    git push
+#else
+#    echo "There is no chart to be updated."
+#fi
 echo "Done."
