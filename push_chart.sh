643146902d88ddbe5137ce49b8a243a8fc1327ae
Nam Phuong <phuongnam.nextx@gmail.com>
2022-12-20 16:32:50 +0700

    update readme file

--- push_chart.sh
+++ push_chart.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 echo "Begin"
 
-helm package charts/java-application-chart
-
-FILES="*.tgz"
+FILES="$1/*.tgz"
 export isChartChange=0
 
 echo "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" | helm registry login -u namnextx --password-stdin ghcr.io
@@ -19,19 +17,19 @@ echp isChartChange
 
 #echo $isChartChange
 #
-#if [ $isChartChange = 1 ];
-#then
-#    echo "Updating the index.yaml..."
-#    git remote set-url origin "https://${GITHUB_TOKEN}:${GITHUB_TOKEN}@github.com/${OWNER_REPO}/${REPO_NAME}"
-#    git reset --hard
-#    git checkout chart-releases
-#    git pull
-#    helm repo index ${CHART_PACKAGE_PATH} --merge index.yaml --url oci://${CHART_REGISTRY}/${OWNER_REPO}
-#    mv -f "${CHART_PACKAGE_PATH}/index.yaml" "index.yaml"
-#    git add index.yaml
-#    git commit -m "Update index..."
-#    git push
-#else
-#    echo "There is no chart to be updated."
-#fi
+if [ $isChartChange = 1 ];
+then
+    echo "Updating the index.yaml..."
+    git remote set-url origin "https://${GITHUB_TOKEN}:${GITHUB_TOKEN}@github.com/${OWNER_REPO}/${REPO_NAME}"
+    git reset --hard
+    git checkout chart-releases
+    git pull
+    helm repo index ${CHART_PACKAGE_PATH} --merge index.yaml --url oci://${CHART_REGISTRY}/${OWNER_REPO}
+    mv -f "${CHART_PACKAGE_PATH}/index.yaml" "index.yaml"
+    git add index.yaml
+    git commit -m "Update index..."
+    git push
+else
+    echo "There is no chart to be updated."
+fi
 echo "Done."
